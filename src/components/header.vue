e491eb9e489552b984312a67f9560961c0625743
colodoo <lodog1@qq.com>
2018-08-30 09:11:11 +0800

    遗漏的头部组件

---
+++ src/components/header.vue
@@ -0,0 +1,45 @@
+<template>
+  
+  <header>
+    <a @click="back"><span class="iconfont icon-left"></span></a>
+    <a href="#">{{title}}</a>
+    <a><span class="iconfont icon-gengduo"></span></a>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: 'iheader',
+  props: {
+    title: {
+      type: String,
+      default: '标题'
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    back() {
+      window.history.go(-1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  justify-content: space-between;
+  display: flex;
+  background-color: #96bda8;
+  min-height: 40px;
+  line-height: 40px;
+}
+
+header a {
+  color: #ffffff;
+  font-size: 14px;
+}
+</style>
